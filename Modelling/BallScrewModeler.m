b6052991647ef0ad9bd841c8b602a8be4863889b
Hillenbrand <Hillenbrand@wbk.uni-karlsruhe.de>
2021-05-12 17:15:38 +0200

    Update BallScrewModeler.m

--- Modelling/BallScrewModeler.m
+++ Modelling/BallScrewModeler.m
@@ -3,6 +3,11 @@ classdef BallScrewModeler < Modeler
     
     properties
         ballscrew = BallScrew.empty
+        
+        % csv column defs
+        timeColumn = 1;
+        speedColumn = 2;
+        forceColumn = 5;
     end
     
     methods
@@ -15,20 +20,26 @@ classdef BallScrewModeler < Modeler
     methods 
         function newData = transform(obj, data)
             
-            ts = data(:, 1);
-            sp = data(:, 2);
-            fa = data(:, 5) * 1000;
+            ts = data(:, obj.timeColumn);
+            sp = data(:, obj.speedColumn);
+            fa = data(:, obj.forceColumn) * 1000;
+            % check if data is 1D or 2D
+            if isscalar(ts)           
             
+                obj.ballscrew.updateLoad(ts, sp, fa);
+                obj.ballscrew.updateL10();
+                obj.ballscrew.updateRUL();
+
+                if obj.ballscrew.EndTimeStamp == 0
+                    obj.ballscrew.EndTimeStamp = ts;
+                end
             
-            obj.ballscrew.updateLoad(ts, sp, fa);
-            obj.ballscrew.updateL10();
-            obj.ballscrew.updateRUL();
-            
-            if obj.ballscrew.EndTimeStamp == 0
-                obj.ballscrew.EndTimeStamp = ts;
+                newData = [ts, obj.ballscrew.L_10, obj.ballscrew.Revolutions / obj.ballscrew.L_10 * 100];
+            elseif isvector(ts)
+                obj.ballscrew.updateLifeCycleInformation(ts, fa, sp);
+                
+                newData = [mean(ts), obj.ballscrew.L_10, obj.ballscrew.Revolutions / obj.ballscrew.L_10 * 100];
             end
-            
-            newData = [ts, obj.ballscrew.L_10, obj.ballscrew.Revolutions / obj.ballscrew.L_10 * 100];            
         end
     end
 end
