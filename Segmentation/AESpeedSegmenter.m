875ff36f4a8191628deb1091a946412c509941af
Hillenbrand <Hillenbrand@wbk.uni-karlsruhe.de>
2021-07-06 09:14:42 +0200

    Update AESpeedSegmenter.m
    added option

--- Segmentation/AESpeedSegmenter.m
+++ Segmentation/AESpeedSegmenter.m
@@ -6,6 +6,7 @@ classdef AESpeedSegmenter < Segmenter
         minWindowSize = 6;
         minSpeed = 10;
         tau = 2.5;
+        returnAllSegmentData = false;
     end
     
     methods
@@ -26,26 +27,49 @@ classdef AESpeedSegmenter < Segmenter
                 speed_sections = Mapping.findConstantSections([t_plc, s_plc], obj.tau, obj.minWindowSize, obj.minSpeed);
                 
                 if ~isempty(speed_sections)
-                    ae_sections = cell(size(speed_sections));
-                    speeds = zeros(size(speed_sections));
-                    for s = 1 : length(speed_sections)
-                        sd = speed_sections{s, 1};
-                        ts1 = sd(1, 1);
-                        ts2 = sd(end, 1);
-                        aeInd = t_ae >= ts1 & t_ae <= ts2;
-                        ds_ae = d_ae(aeInd);
-                        ae_sections{s, 1} = ds_ae;
-                        speeds(s, 1) = floor(mean(abs(sd(:, 2))));  % take mean and absolute values
-                    end
-                    if iscell(ae_sections)
-                        if length(ae_sections) > 1
-                            newData = {ae_sections, speeds};
-                        else
-                            newData = [ae_sections, speeds];
+                    if obj.returnAllSegmentData
+                        ae_sections = cell(size(speed_sections, 1), 2);
+                        for s = 1 : length(speed_sections)
+                            sd = speed_sections{s, 1};
+                            ts1 = sd(1, 1);
+                            ts2 = sd(end, 1);
+                            aeInd = t_ae >= ts1 & t_ae <= ts2;
+                            speedInd = t_plc >= ts1 & t_plc <= ts2;
+                            ds_ae = d_ae(aeInd);
+                            ts_ae = t_ae(aeInd);
+                            ae_sections{s, 1} = [ds_ae, ts_ae];
+                            ae_sections{s, 2} = sd;
                         end
                     else
-                        error('unkown ae section output')
-                    end
+                        ae_sections = cell(size(speed_sections));
+                        speeds = zeros(size(speed_sections));
+                        for s = 1 : length(speed_sections)
+                            sd = speed_sections{s, 1};
+                            ts1 = sd(1, 1);
+                            ts2 = sd(end, 1);
+                            aeInd = t_ae >= ts1 & t_ae <= ts2;
+                            ds_ae = d_ae(aeInd);
+                            ae_sections{s, 1} = ds_ae;
+                            speeds(s, 1) = floor(mean(abs(sd(:, 2))));  % take mean and absolute values                        
+                        end
+                        if iscell(ae_sections)
+                            if length(ae_sections) > 1
+                                if ~obj.returnAllSegmentData
+                                    newData = {ae_sections, speeds};
+                                else
+                                    newData = ae_sections;
+                                end
+                            else
+                                if ~obj.returnAllSegmentData
+                                    newData = [ae_sections, speeds];
+                                else
+                                    newData = ae_sections;
+                                end
+                            end
+                        else
+                            error('unkown ae section output')
+                        end
+                    end                    
                 else
                     newData = [];
                 end
