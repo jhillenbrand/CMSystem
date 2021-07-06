a3e47e0fc93705d2750ba6da22bf90f138475a49
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2021-07-06 20:18:54 +0200

    Update AESpeedFilter.m

--- Preprocessing/AEPreprocessor/AESpeedFilter.m
+++ Preprocessing/AEPreprocessor/AESpeedFilter.m
@@ -2,48 +2,46 @@ classdef AESpeedFilter < Preprocessor
     %AESpeedFilter 
     
     properties
+        minSpeed = 0;
+        lowerLim = 0;
+        upperLim = 0;
+        sampleRate = 0;
     end
     
     methods
-        function obj = AESpeedFilter(minSpeed, lowerLim, upperLim)
+        function obj = AESpeedFilter(minSpeed, lowerLim, upperLim, sampleRate)
             %AESpeedFilter 
-            if nargin < 5
+            if nargin < 4
                 error('not enough input arguments')
             end
-            obj@Preprocessor([class(AESpeedFilter.empty) ' [' char(java.util.UUID.randomUUID().toString()) ']'], []);
-            
-            funcHandle = @(x) AEPreprocessor.filterFunc(x, minSpeed, lowerLim, upperLim);
-            trafo = Transformation('AESpeedFilter', funcHandle);
-            obj.addTransformation(trafo);
+            obj@Preprocessor([class(AESpeedFilter.empty) ' [' char(java.util.UUID.randomUUID().toString()) ']'], []);           
+            obj.minSpeed = minSpeed;
+            obj.lowerLim = lowerLim;
+            obj.upperLim = upperLim;
+            obj.sampleRate = sampleRate;
         end       
     end
     
-    methods (Static)
-        function newData = filterFunc(data, minSpeed, lowerLim, upperLim)
-            %filterFunc(data, minSpeed, lowerLim, upperLim)
-            newData = data;
-            if ~isempty(data)
-                t_plc = data{3, 1};
+    %% Interface EMethods
+    methods
+        function newData = transform(obj, data)
+            %filterFunc(data, minSpeed, lowerLim, upperLim, sampleRate)
+            if ~isempty(data)           
+                %t_plc = data{3, 1};
                 s_plc = data{4, 1};
                 t_ae = data{1, 1};
                 d_ae = data{2, 1};
-                sInd = (s_plc > minSpeed);
+                sInd = (s_plc > obj.minSpeed);
                 if sum(sInd) > 1
-                    newData = SignalAnalysis.removeWindowsInRange(d_ae, stillstandLowerLimit, stillstandUpperLimit, ceil(sampleRate * 0.1), false);            
+                    [d_ae_r, inds] = SignalAnalysis.removeWindowsInRange(d_ae, obj.lowerLim, obj.upperLim, ceil(obj.sampleRate * 0.05), false);            
+                    t_ae_r = t_ae(inds);
+                    newData = [t_ae_r, d_ae_r];
                 else
                     newData = [];
                 end
             else
                 newData = [];
-            end
-            if removeStillstand
-                % remove stillstand sections if present defined by noise
-                % lower and upper limit
-                newData = SignalAnalysis.removeWindowsInRange(newData, stillstandLowerLimit, stillstandUpperLimit, ceil(sampleRate * 0.1), false);
-            end
-            if isempty(newData)
-                newData = [NaN; NaN];
-            end
+            end            
         end
     end
 end
