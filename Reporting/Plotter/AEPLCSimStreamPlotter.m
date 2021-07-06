254c0d5fa8724b14bf309f0ca4046e46a280fa3a
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2021-07-06 19:27:43 +0200

    Update AEPLCSimStreamPlotter.m

--- Reporting/Plotter/AEPLCSimStreamPlotter.m
+++ Reporting/Plotter/AEPLCSimStreamPlotter.m
@@ -2,9 +2,6 @@ classdef AEPLCSimStreamPlotter < Plotter
     %AEPLCSimStreamPlotter 
     
     properties
-        simStreamAcquisitor = [];
-        fileFieldInds = [];
-        plotCounter = 0;
     end
     
     methods
@@ -16,25 +13,27 @@ classdef AEPLCSimStreamPlotter < Plotter
     %% Interface Methods
     methods
         function report(obj, data)
-            if iscell(data)
-                t_ae = data{1,1};
-                d_ae = data{2, 1};
-                t_plc = data{3, 1};
-                d_plc = data{4, 1};
-                yyaxis right
-                    plot(t_ae, d_ae)
-                    ylabel('AE [-]')
-                
-                yyaxis left
-                    plot(t_plc, d_plc);
-                    ylabel('Speed [1/min]')
-                
-                xlabel('Time [ms]')
-                grid on
-                grid minor
-                drawnow
-            else
-                error(['data[' class(data) '] must be of type cell'])
+            if ~isempty(data)
+                if iscell(data)
+                    t_ae = data{1, 1};
+                    d_ae = data{2, 1};
+                    t_plc = data{3, 1};
+                    d_plc = data{4, 1};
+                    yyaxis right
+                        plot(t_ae, d_ae)
+                        ylabel('AE [-]')
+
+                    yyaxis left
+                        plot(t_plc, d_plc);
+                        ylabel('Speed [1/min]')
+
+                    xlabel('Time [ms]')
+                    grid on
+                    grid minor
+                    drawnow
+                else
+                    error(['data[' class(data) '] must be of type cell'])
+                end
             end
         end
     end
